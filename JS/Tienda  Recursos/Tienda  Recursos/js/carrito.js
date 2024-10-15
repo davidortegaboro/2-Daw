285b34a3afa0f37bcb7819419eb7572e41c11dc5
David <davidortegaboro6667@gmail.com>
2024-10-15 20:45:34 +0200

    js

--- JS/Tienda  Recursos/Tienda  Recursos/js/carrito.js
+++ JS/Tienda  Recursos/Tienda  Recursos/js/carrito.js
@@ -1,16 +1,23 @@
-class Carrito{
-	constructor(id){						
+class Carrito {
+
+	_id;
+	_listaAriculos;
+
+	constructor(id) {
+		this.id = id;
+		this._listaAriculos = [];
+
 	}
-						
-	anyadeArticulo(articulo){		
-		}			
-				
-	borraArticulo(codigo){		
+
+	anyadeArticulo(articulo) {
 	}
-	
-	modificaUnidades(codigo,n){		
-	}	
-			
-	verCarrito(){
-	}			
+
+	borraArticulo(codigo) {
+	}
+
+	modificaUnidades(codigo, n) {
+	}
+
+	verCarrito() {
+	}
 }
--- JS/Tienda  Recursos/Tienda  Recursos/js/tienda.js
+++ JS/Tienda  Recursos/Tienda  Recursos/js/tienda.js
@@ -1,29 +1,52 @@
-	criterios=["Sin ordenar","Ascendente por precio", "Descendente por precio"]
-	
-	function creaListaCriterios(){
-		
+criterios = ["Sin ordenar", "Ascendente por precio", "Descendente por precio"]
+
+
+function creaListaCriterios() {
+	//Guarda el select del HTML en selectProductos
+	let selectProductos = document.getElementById("criteriosOrdenacion");
+
+	//Voy creando los option y los guardo en opcionesSelect, luego la inserto en el HTML
+	let opcionesSelect = ``;
+	for (let i = 0; i < criterios.length; i++) {
+		opcionesSelect += `<option value="${i}">${criterios[i]}</option>`;
+	}
+	selectProductos.innerHTML = opcionesSelect;
+
+	// Añadir el evento change
+	selectProductos.addEventListener("change", function () {
+		pintaArticulos(selectProductos.value);
+	});
+}
+
+
+
+function pintaArticulos(orden) {
+	//Siguiente metodo
+	if (orden === 0) {
+	} else if (orden === 1) {
+	} else {
 	}
 
-
-	function pintaArticulos(orden){
-		
-	}
-	
-	
-	function ponArticuloEnCarrito(){
-		
-	}
+}
 
 
-	function verCarro(){
-	
-	}
+function ponArticuloEnCarrito() {
 
-	function efectuaPedido(){
-	
-	}
+}
 
-	window.onload=()=>{
 
-	}
+function verCarro() {
+
+}
+
+function efectuaPedido() {
+
+}
+
+
+window.onload = () => {
+	creaListaCriterios();
+
+}
+
 
