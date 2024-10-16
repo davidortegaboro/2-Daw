7f3bab5707899f5c4206a295f2ffe889c6cd55c0
David <davidortegaboro6667@gmail.com>
2024-10-16 10:39:39 +0200

    pintarArticulos

--- JS/Tienda  Recursos/Tienda  Recursos/js/tienda.js
+++ JS/Tienda  Recursos/Tienda  Recursos/js/tienda.js
@@ -1,4 +1,6 @@
-criterios = ["Sin ordenar", "Ascendente por precio", "Descendente por precio"]
+import { listaArticulos } from "./datos.js";
+
+let criterios = ["Sin ordenar", "Ascendente por precio", "Descendente por precio"]
 
 
 function creaListaCriterios() {
@@ -18,16 +20,58 @@ function creaListaCriterios() {
 	});
 }
 
-
+const copiaLista = [...listaArticulos];
 
 function pintaArticulos(orden) {
-	//Siguiente metodo
-	if (orden === 0) {
-	} else if (orden === 1) {
-	} else {
+	let innerArticulos = ``;
+	let contenedorArticulos = document.getElementById("contenedor");
+	let opcionLista = [];  // Inicializamos opcionLista como un array vacío
+
+	switch (orden) {
+		// sin ordenar
+		case "0":
+			opcionLista = [...copiaLista]; // Clonamos la lista original para no modificarla
+			break;  // Añadimos el break para detener el flujo aquí
+
+		// ascendente
+		case "1":
+			listaArticulos.sort((a, b) => a.precio - b.precio);
+			opcionLista = listaArticulos;
+			break;
+
+		// descendente
+		case "2":
+			listaArticulos.sort((a, b) => b.precio - a.precio);
+			opcionLista = listaArticulos;
+			break;
+
+		default:
+			opcionLista = [...copiaLista];  // Si no coincide ningún caso, usamos la lista original
 	}
 
+	// Ahora pintamos los artículos de opcionLista
+	opcionLista.forEach(a => {
+		innerArticulos += `
+        <div class="col">
+            <div class="card">
+                <img src="assets/${a.codigo}.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${a.nombre}</h5>
+                    <p class="card-text">${a.descripcion}</p>
+                    <b>
+                    <p class="card-text text-center">${a.precio}$</p>
+                    </b>
+                </div>
+                <button id="m1" class="btn-success">comprar</button>
+            </div>
+        </div>`;
+	});
+
+	// Asignamos el HTML generado al contenedor de artículos
+	contenedorArticulos.innerHTML = innerArticulos;
 }
+
+
 
 
 function ponArticuloEnCarrito() {
@@ -45,6 +89,7 @@ function efectuaPedido() {
 
 
 window.onload = () => {
+	pintaArticulos(0);
 	creaListaCriterios();
 
 }
